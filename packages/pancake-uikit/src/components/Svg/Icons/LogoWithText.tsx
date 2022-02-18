4f966fc928475ef77f10ebceb32e17d9eba96393
top1st <riguang102@gmail.com>
2022-02-18 20:54:55 +0800

    detrix logo size

--- packages/pancake-uikit/src/components/Svg/Icons/LogoWithText.tsx
+++ packages/pancake-uikit/src/components/Svg/Icons/LogoWithText.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Svg from "../Svg";
 import { SvgProps } from "../types";
+import { MENU_HEIGHT } from "../../../widgets/Menu/config";
 
 interface LogoProps extends SvgProps {
   isDark: boolean;
@@ -9,8 +10,8 @@ interface LogoProps extends SvgProps {
 const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
-    <Svg viewBox="0 0 160 26" {...props}>
-      <image href="/logo.png"/>
+    <Svg viewBox="0 0 256 178" width={MENU_HEIGHT} height={MENU_HEIGHT} {...props}>
+      <image href="/logo-text.png"/>
     </Svg>
   );
 };
