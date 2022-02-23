47a688daa7e9d2088fac984f2b93da7d06c15838
top1st <riguang102@gmail.com>
2022-02-23 22:14:54 +0800

    metaprofitsdao-dist

---
+++ packages/pancake-uikit/metaprofitsdao-dist/components/Footer/config.d.ts
@@ -0,0 +1,5 @@
+import { Language } from "../LangSelector/types";
+import { FooterLinkType } from "./types";
+export declare const footerLinks: FooterLinkType[];
+export declare const socials: any[];
+export declare const langs: Language[];
